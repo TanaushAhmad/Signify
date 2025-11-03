f4c26fd5c461028adc26b8b3c202c38875fa20e9
Tanaush <154371641+TanaushAhmad@users.noreply.github.com>
2025-11-03 22:04:15 +0400

    Create preprocessing.py

---
+++ backend/app/utils/preprocessing.py
@@ -0,0 +1,6 @@
+
+SIGN_MAPPINGS = {
+    "ASL": {"HELLO": "wave_right_hand", "THANK_YOU": "chin_to_hand"},
+    "BSL": {"HELLO": "two_hand_wave", "THANK_YOU": "flat_hand_lead"},
+    "ISL": {"HELLO": "palm_up_wave", "THANK_YOU": "touch_chin_then_forward"}
+}
